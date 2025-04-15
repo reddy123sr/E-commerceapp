9dcef3203a0f0e2d5735adc300e9549c54932fb3
srikanth reddy <9920004208@klu.ac.in>
2025-04-15 10:03:13 +0530

    updated changes

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useCart } from './CartContext';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { removeFromwishlist } from '../redux/wishlistSlice';
